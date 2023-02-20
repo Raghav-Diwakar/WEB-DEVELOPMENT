6397e998c02c9da0a3cac823501829b19de38005
RAGHAVJIMZN <raghavdiwakar17@gmail.com>
2023-02-20 20:12:38 +0530

    /
    /

---
+++ PHP/notes.php
@@ -0,0 +1,35 @@
+/** 
+// single line comment 
+# this is also single line comment 
+/* this is multi line comment */
+
+Variable ( case Sensitive a , A are different)
+$new = 22 ;
+start with $ sign 
+you have to assign value with declaration 
+
+output variable - echo 
+echo "18+4=".$new."!";
+
+*  3 types of scope - global , local , static 
+
+* D/B echo and print 
+echo  doesn't have return value but print have value of 1 
+echo can take  multiple parameters but print only 1 
+echo is manually faster 
+
+* data types 
+string = "string ";
+int b/w 2^-31 to 2^31
+float $f = 112.244
+boolean $x = true / false 
+array => cars = array("1","2","3")
+null , $x = null ;
+class and object  
+*/
+
+* length of string => strlen() ;
+* count words => str_word_count ;
+reverse => strrev("hi") ;
+search word =>  strpos("Hello","lo") ;
+replace => str_replace("hello","hi","hello World")
